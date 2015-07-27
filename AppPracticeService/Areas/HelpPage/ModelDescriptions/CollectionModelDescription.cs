f37547b5194dd8ff5f53317ab567cf99645b8b8f
Tri Nguyen Minh <tringuyenminh92@gmail.com>
2015-07-27 17:36:55 +0700

    init project web api for app practice

---
+++ AppPracticeService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AppPracticeService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AppPracticeService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AppPracticeService.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
